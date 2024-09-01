2cde9d1f8140c52f0f92ba18add567d45e9387c8
Afsal Thaj <afsal.taj06@gmail.com>
2024-09-02 00:23:14 +1000

    See if things work

--- src/remote.rs
+++ src/remote.rs
@@ -5,7 +5,8 @@ use serde_json::json;
 pub fn worker_to_worker() -> core::result::Result<String, String> {
     let url = "https://x.api.golem.cloud/api/v3/user";
 
-    let client = Client::builder().build().expect("Failed to create client");
+    let client =
+        Client::builder().build().map_err(|e| format!("Failed to create client: {}", e))?;
 
     // JSON body
     let json_body = json!({
